ecc4af11158603e1f1d0e5e5b45ee54867f45f29
Skashe <johan.silenthill@gmail.com>
2020-11-12 19:21:46 +0100

    JavaFX implemented with sample Project. Changes needed

--- zuul-framework/zuul-framework/worldofzuul/Main.java
+++ zuul-framework/zuul-framework/worldofzuul/Main.java
@@ -1,9 +1,21 @@
 package worldofzuul;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
-
-    public static void main(String[] yolo) {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage.setTitle("Hello World");
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
         Game myGame = new Game();
 
         myGame.play();
