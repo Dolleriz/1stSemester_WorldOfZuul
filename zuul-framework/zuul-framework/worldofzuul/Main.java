1618469007ef7e1f99c8ea687698f831054a485d
Johan <johan.silenthill@gmail.com>
2020-11-17 15:50:21 +0100

    fixed game crash (GUI now draws, but is unfinished)

--- zuul-framework/zuul-framework/worldofzuul/Main.java
+++ zuul-framework/zuul-framework/worldofzuul/Main.java
@@ -3,16 +3,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        stage.setTitle("Hello World");
-        stage.setScene(new Scene(root, 300, 275));
-        stage.show();
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("sample.fxml"));
+        Stage root = loader.load();
+        root.show();
+        //Scene scene = new Scene(root, 750, 750);
+        //stage.setTitle("World of Trash");
+        //stage.setScene(scene);
+        //stage.show();
     }
     public static void main(String[] args) {
         launch(args);
