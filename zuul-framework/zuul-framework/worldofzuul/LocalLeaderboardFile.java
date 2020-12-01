f62db67d3218c86e68756b89956be77af5267944
Mikkel Dolleris Christensen <mikch19@gmail.com>
2020-12-01 11:07:43 +0100

    Made changes to LocalLeaderboardFile.java

--- zuul-framework/zuul-framework/worldofzuul/LocalLeaderboardFile.java
+++ zuul-framework/zuul-framework/worldofzuul/LocalLeaderboardFile.java
@@ -1,10 +1,36 @@
 package worldofzuul;
 
 import java.io.File;
-import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class LocalLeaderboardFile {
+public class LocalLeaderboardFile(){
+
+    public LocalLeaderboardFile(Game game) {
+    }
+
+    public void createFile () throws IOException{
+        File leaderboardFile = new File("Leaderboard.txt");
+
+        try {
+            if (!leaderboardFile.createNewFile()) {
+                overwriteFile(leaderboardFile);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void overwriteFile(File leaderboardFile) throws IOException{
+        FileWriter leaderboardWriter = null;
+
+        leaderboardWriter = new FileWriter("Leaderboard.txt");
 
 
+        if () {
+            leaderboardWriter.write("Highscore: " + );
+
+        }
+    }
 }
 
