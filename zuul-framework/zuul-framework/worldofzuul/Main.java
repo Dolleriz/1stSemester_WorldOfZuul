2c2195ee05764198ac105f0937e3a677fd89581a
Johan <johan.silenthill@gmail.com>
2020-12-01 10:47:37 +0100

    Added into screen and fixed pathworking bugs

--- zuul-framework/zuul-framework/worldofzuul/Main.java
+++ zuul-framework/zuul-framework/worldofzuul/Main.java
@@ -3,6 +3,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -17,6 +18,13 @@ public class Main extends Application {
         FXMLLoader loader= new FXMLLoader(getClass().getResource("outside.fxml"));
         Stage root = loader.load();
         root.show();
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("En verden af Skrald");
+        alert.setHeaderText(null);
+        alert.setContentText("Velkommen til En Verden af Affald! \nDette er et nyt spændende affaldsindsamlingsspil." +
+                "\nTryk på hjælp, hvis du ikke hvad hvad du skal!");
+
+        alert.showAndWait();
         //Scene scene = new Scene(root, 750, 750);
         //stage.setTitle("World of Trash");
         //stage.setScene(scene);
