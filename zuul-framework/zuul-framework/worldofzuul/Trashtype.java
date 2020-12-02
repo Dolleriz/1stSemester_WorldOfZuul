e8968f5d4bf8aaa38f501a910a9e7a4fd8b1edd6
Sandra <small11@student.sdu.dk>
2020-12-02 11:41:44 +0100

    Auto stash before merge of "GUI_RPG_testing" and "origin/GUI_RPG_testing"

--- zuul-framework/zuul-framework/worldofzuul/Trashtype.java
+++ zuul-framework/zuul-framework/worldofzuul/Trashtype.java
@@ -1,23 +1,34 @@
 package worldofzuul;
 
+import javafx.scene.image.Image;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 
 public enum Trashtype {
 
-    PLASTIC("Plastik."),
+    FOOD_WASTE("Madaffald.", "img\\trash.png"),
 
-    METAL_AND_GLASS("Metal og glas."),
+    METAL_AND_GLASS("Metal og glas.", "img\\trash.png"),
 
-    FOOD_WASTE("Madaffald."),
+    PAPER("Papir og pap.", "img\\trash.png"),
 
-    PAPER("Papir og pap."),
+    PLASTIC("Plastik.", "img\\trash.png"),
 
-    RESIDUAL_WASTE("Restaffald.");
+    RESIDUAL_WASTE("Restaffald.", "img\\trash.png");
 
     private String trashString;
+    Image image;
 
-    Trashtype(String trashType){
+    Trashtype(String trashType, String filePath) {
         this.trashString = trashType;
+        try{
+        this.image = new Image(new FileInputStream(filePath));}
+        catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
     }
 
     public String toString() {
